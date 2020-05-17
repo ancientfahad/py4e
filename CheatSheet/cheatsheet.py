bcfe0c2fbd14c499d4667cdb7187f50a0c6b66d5
Fahad Chowdhury <fchowdhury137@gmail.com>
2020-05-18 01:03:19 +0600

    Using Python to Access Web Data - Week 2

--- CheatSheet/cheatsheet.py
+++ CheatSheet/cheatsheet.py
@@ -548,3 +548,108 @@ print(sorted([(v, k) for k, v in d.items()]))
 
 days = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
 print(days[2])
+
+# Regular Expression
+import re
+
+# re.search() returns T/F if string matches regex
+# re.findall() returns the matching strings
+
+sample_text = 'I finished my SSC on 2011, with a GPA of 4.75, with 5 A+'
+sample_text = 'From: Using the: character'
+
+find_num = re.findall('[0-9]+', sample_text)  # one or more digits
+find_vowel = re.findall('[aeiouAEIOU]+', sample_text)  # one or more characters (vowel)
+
+greedy_matching = re.findall('^F.+:', sample_text)
+nongreedy_matching = re.findall('^F.+?:', sample_text)
+
+print(find_num)
+print(find_vowel)
+print(greedy_matching)
+print(nongreedy_matching)
+
+
+def re_func():
+    hand = open('/Users/u75530/Documents/mbox-short.txt')
+    for line in hand:
+        line = line.rstrip()
+
+        # Search
+        # if re.search('From: ', line):
+
+        # Starts with F
+        # if re.search('^From: ', line):
+
+        # Starts with X - has any number of characters - has a colon (:)
+        # if re.search('^X.*:', line):
+
+        # Starts with X - has dash (-) - has any non-whitespace character - has a colon (:)
+        if re.search('^X-\S+:', line):
+            print(line)
+
+
+def find_func():
+    hand = open('/Users/u75530/Documents/mbox-short.txt')
+    for line in hand:
+        line = line.rstrip()
+        if line.find('From: ') >= 0:
+            print(line)
+
+
+re_func()
+find_func()
+
+data = 'From fahad.chowdhury@aisdhaka.org Sat Jan  5 09:14:16 2008'
+
+# Extracting email address using regex
+email_address = re.findall('\S+@+\S+', data)  # \S+ one or more non-whitespace character
+email_address = re.findall('^From (\S+@+\S+)', data)  # Parenthisis () extracts the data we want
+print(email_address)
+
+# Extracting host manually
+at_position = data.find('@')
+space_after_at_position = data.find(' ', at_position)
+host = data[at_position + 1:space_after_at_position]
+print(host)
+
+# Extracting host with regex
+# host = re.findall('@+(\S+)', data)
+# host = re.findall('@([^ ]*)', data)  # [^ ] everything but space * one or more
+host = re.findall('^From .*@([^ ]*)', data)  # .* any number of characters
+print(host)
+
+# Extracting host with dual split
+data_split = data.split()
+email = data_split[1]
+email_split = email.split('@')
+host = email_split[1]
+print(host)
+
+file_handler = open('/Users/u75530/Documents/mbox-short.txt')
+count = 0
+for line in file_handler:
+    host = re.findall('^From .*@[^ ]*', line)
+    email = re.findall('^From (\S+@+\S+)', line)
+
+    if len(host) > 0:
+        count = count + 1
+        print(host)
+print(count)
+
+# X-DSPAM-
+file_handler = open('/Users/u75530/Documents/mbox-short.txt')
+data_list = list()
+
+for line in file_handler:
+    data = re.findall('^X-DSPAM-Confidence: (\S+)', line)
+    data = re.findall('^X-DSPAM-Confidence: ([0-9.]+)', line)
+    if len(data) > 0:
+        data_list.append(float(data[0]))
+
+print('Max:', max(data_list))
+
+# Use of backslash \
+data = 'This game will cost $10.00'
+get_price = re.findall('\$[0-9.]+', data)
+print(get_price)
